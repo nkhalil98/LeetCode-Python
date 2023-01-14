55157b140371ed5af4a775bf41ab789edcdebd6a
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2023-01-14 03:09:35 -0500

    Update 1491.py

--- Programming Skills I/1491.py
+++ Programming Skills I/1491.py
@@ -18,5 +18,5 @@ def average(salary: list[int]) -> float:
     return sum(salary) / len(salary)
 
 if __name__ == "__main__":
-    print(average(salary = [4000,3000,1000,2000])) # 2500
-    print(average(salary = [1000,2000,3000])) # 2000
+    assert average(salary = [4000,3000,1000,2000]) == 2500
+    assert average(salary = [1000,2000,3000]) == 2000
