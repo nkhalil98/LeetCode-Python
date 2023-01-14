ac3a72640099c3276acf781944e26c2ac760a37a
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2023-01-14 03:07:14 -0500

    Update 1532.py

--- Programming Skills I/1532.py
+++ Programming Skills I/1532.py
@@ -2,6 +2,10 @@
 1523. Count Odd Numbers in an Interval Range
 
 Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+Constraints:
+
+0 <= low <= high <= 10^9
 """
 
 def countOdds(low: int, high: int) -> int:
