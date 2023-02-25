9354eb6ed7be9f0822a441b57ddc6b02abc5c884
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2023-02-25 15:10:12 -0500

    Add files via upload

---
+++ 1480.py
@@ -0,0 +1,25 @@
+"""
+1480. Running Sum of 1d Array
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+Constraints:
+
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+"""
+
+def runningSum(nums: list[int]) -> list[int]:
+        running = []
+        n = len(nums)
+        for i in range(n):
+            running.append(sum(nums[: i+1]))
+        return running
+
+if __name__ == "__main__":
+    # test cases
+    assert runningSum([1,2,3,4]) == [1,3,6,10]
+    assert runningSum([1,1,1,1,1]) == [1,2,3,4,5]
+    assert runningSum([3,1,2,10,1]) == [3,4,6,16,17]
