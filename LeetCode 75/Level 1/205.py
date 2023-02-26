62fbfebde2e7e72d389320f6f77440239bc281da
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2023-02-26 11:27:15 -0500

    Add files via upload

---
+++ LeetCode 75/Level 1/205.py
@@ -0,0 +1,35 @@
+"""
+205. Isomorphic Strings
+
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+Constraints:
+
+1 <= s.length <= 5 * 10^4
+t.length == s.length
+s and t consist of any valid ascii character.
+"""
+
+def isIsomorphic(s: str, t: str) -> bool:
+        mapping = {}
+        n = len(s)
+        for i in range(n):
+            if s[i] not in mapping:
+                if t[i] not in mapping.values():
+                    mapping[s[i]] = t[i]
+                else:
+                    return False
+            else:
+                if mapping[s[i]] != t[i]:
+                    return False
+        return True
+
+if __name__ == "__main__":
+    # test cases
+    assert isIsomorphic("egg", "add") == True
+    assert isIsomorphic("foo", "bar") == False
+    assert isIsomorphic("paper", "title") == True
