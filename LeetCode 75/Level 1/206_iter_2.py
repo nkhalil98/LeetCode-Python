74c101004f33bc9b68d0412433298a0a66f82b8f
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2023-02-26 14:15:02 -0500

    Add files via upload

---
+++ LeetCode 75/Level 1/206_iter_2.py
@@ -0,0 +1,32 @@
+"""
+206. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+def reverseList(self, head: ListNode) -> ListNode:
+    prev = None
+    curr = head
+    while curr:
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+    return prev   
+
+if __name__ == "__main__":
+    # test cases
+    pass
